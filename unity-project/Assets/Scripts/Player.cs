b45c2278219271297383810076d6ede5fdee16db
MJ <MJaSchnee@gmail.com>
2025-02-07 23:10:55 -0500

    Player turn indicator moves with player

--- unity-project/Assets/Scripts/Player.cs
+++ unity-project/Assets/Scripts/Player.cs
@@ -19,13 +19,14 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-       GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-       cylinder.transform.position = transform.position;
-       cylinder.transform.position += new Vector3(0,-.5f,0);
-       cylinder.transform.localScale += new Vector3(.5f,-.95f,.5f);
-       
-       // Get the Renderer component from the new cube
-       turnIdentifierRenderer = cylinder.GetComponent<Renderer>();
+        GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        cylinder.transform.parent = transform;
+        cylinder.transform.position = transform.position;
+        cylinder.transform.position += new Vector3(0,-.5f,0);
+        cylinder.transform.localScale += new Vector3(.5f,-.95f,.5f);
+        
+        // Get the Renderer component from the new cube
+        turnIdentifierRenderer = cylinder.GetComponent<Renderer>();
     }
 
     void OnEndTurn(Player nextPlayer)
